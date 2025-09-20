4a152847023851b8f1ea5de09bf83085efdb4819
SHYoussef <153778481+SHYoussef@users.noreply.github.com>
2025-09-20 16:53:23 +0200

    chore: add interfaces

---
+++ src/reflex-rag/api/api.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+
+
+#TODO: define route for response generation
+class FastAPIendpoint:
+    def __init__(self) -> None:
+        self.app = FastAPI()
---
+++ src/reflex-rag/graph/interfaces/node.py
@@ -0,0 +1,24 @@
+from typing import Annotated
+
+from typing_extensions import TypedDict
+
+from langgraph.graph.message import add_messages
+
+import inspect
+
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
+
+
+
+class INode:
+    def __init__(self, state: State):
+        self.all_methods = self.get_all_methods_names
+
+    @classmethod
+    def _get_all_methods_names(cls):
+        return [name for name, func in inspect.getmembers(cls, inspect.isfunction)]     
+
