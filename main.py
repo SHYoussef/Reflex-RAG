52f68587924c14e6340074c1bee6d514bd73574c
SHYoussef <153778481+SHYoussef@users.noreply.github.com>
2025-09-27 00:09:54 +0200

    feat: add fastapi and chainlit front

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-from IPython.display import Image, display
-from reflex.graph.graph import ReflexAgent
-from reflex.graph.nodes import ReflexNodes
+# from IPython.display import Image, display
+# from reflex.agent.graph import ReflexAgent
+# from reflex.agent.nodes import ReflexNodes
 
-nodes = ReflexNodes()
-reflex_agent = ReflexAgent(nodes=nodes)
+# nodes = ReflexNodes()
+# reflex_agent = ReflexAgent(nodes=nodes)
 
 
-try:
-    # Get the PNG data
-    png_data = reflex_agent.graph.get_graph().draw_mermaid_png()
+# try:
+#     # Get the PNG data
+#     png_data = reflex_agent.graph.get_graph().draw_mermaid_png()
     
-    # Save to file
-    with open('./reflex_graph.png', 'wb') as f:
-        f.write(png_data)
+#     # Save to file
+#     with open('./reflex_graph.png', 'wb') as f:
+#         f.write(png_data)
     
-except Exception as e:
-    raise(e)
+# except Exception as e:
+#     raise(e)
+
+from reflex.agent.interfaces.agent import IAgent
+from reflex.api.api import FastAPIendpoint
+from reflex.agent.mock_agent import MockAgent
+from chainlit.utils import mount_chainlit
+  
+agent = MockAgent()
+
+fastapi_app = FastAPIendpoint(agent=agent).app
+
+mount_chainlit(app=fastapi_app, target="./reflex/front/cl_entrypoint.py", path="/interface")
+
